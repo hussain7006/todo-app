a3950fa83774ae107f91ebedabeea3b658ddacfa
hussain <m.hussain7006@gmail.com>
2020-08-13 00:23:21 +0500

    Setup Firebase Database

--- public/app.js
+++ public/app.js
@@ -3,48 +3,84 @@ let todoInput = document.getElementById("todoInput");   // getting ID of input f
 let todoList = document.getElementById("todoList");     // getting Id of UL
 
 function getTodo() {
-
-    // Creating List Start
-    let createList = document.createElement('li');      // creating list tags
     if (todoInput.value) {
-        
-        createList.append(todoInput.value);                 // inserting text into list tags
-
-        let updateButton = document.createElement('button'); // creating update button
-        updateButton.append("Edit");                         // setting button name
-        updateButton.setAttribute("onclick", "editList(this)"); // onclick calling function and passes itself
-        updateButton.setAttribute("class", "updateButton");     // setting class name
-        // createList.append(updateButton);                        // inserting update button into list
-    
-        let deleteButton = document.createElement('button'); // creating button
-        deleteButton.append("Delete");                       // setting button name
-        deleteButton.setAttribute("onclick","delteElement(this.parentNode)"); // onclick calling function and passes its parent node
-        deleteButton.setAttribute("class", "deleteButton");     // setting class name
-        // createList.append(deleteButton);                     // inserting this button into list
-
-        let bothButtonDiv = document.createElement('div');
-        bothButtonDiv.setAttribute("class", "editDelteDiv");     // setting class name
-        bothButtonDiv.append(updateButton);
-        bothButtonDiv.append(deleteButton);
-        createList.append(bothButtonDiv);
-        // console.log(bothButtonDiv);
-
-        todoList.append(createList);                        // inserting list into UL
-        todoInput.value = "";                               // empty the input field
-        // Creating List End
+        firebase.database().ref('App').push(todoInput.value);
+        todoInput.value = "";  // empty the input field
+        deleteAll();
+        fillingList();
     }
 }
 
 function delteElement(e) {
-    e.parentNode.remove();                                         // removing list
+    // e.parentnode.parentNode.remove();  // removing list
+    // console.log(e.value);
+    firebase.database().ref('App/'+e.value).remove();
+    deleteAll();
+    fillingList();
 }
 
 function editList(e){
-    // console.log(e)
     let newTodo = prompt("Enter a new text", e.parentNode.parentNode.firstChild.nodeValue); // getting new todo input from user
-    if (newTodo) e.parentNode.parentNode.firstChild.nodeValue = newTodo;        // upldate old todo item
+    // if (newTodo) e.parentNode.parentNode.firstChild.nodeValue = newTodo;        // upldate old todo item
+    if(newTodo){
+        firebase.database().ref('App/'+e.value).set(newTodo);
+        deleteAll();
+        fillingList();
+    }
+
 }
 
 function deleteAll() {
-    todoList.innerHTML = "";                            // empty the UL
+    todoList.innerHTML = ""; // empty the UL
+}
+
+function deleteAllData() {
+    firebase.database().ref('App').remove();
+    deleteAll();
+    fillingList();
+}
+
+// console.log();
+// firebase.database().ref('/').once('value'function(data){
+//     console.log(data.val())
+// })
+// firebase.database().ref('/').remove();
+fillingList();
+function fillingList(){
+    firebase.database().ref('/').on('child_added',function(data){
+        // console.log(data.val())
+        // console.log(data.val().)
+        for (const [key, value] of Object.entries(data.val())) {
+            // console.log(`${key}: ${value}`);
+
+            // Creating List Start
+            let createList = document.createElement('li');      // creating list tags
+            createList.append(value);                 // inserting text into list tags
+
+            let updateButton = document.createElement('button'); // creating update button
+            updateButton.append("Edit");                         // setting button name
+            updateButton.setAttribute("onclick", "editList(this)"); // onclick calling function and passes itself
+            updateButton.setAttribute("class", "updateButton");     // setting class name
+            updateButton.setAttribute("value", key);
+            // createList.append(updateButton);                        // inserting update button into list
+        
+            let deleteButton = document.createElement('button'); // creating button
+            deleteButton.append("Delete");                       // setting button name
+            deleteButton.setAttribute("onclick","delteElement(this)"); // onclick calling function and passes its parent node
+            deleteButton.setAttribute("class", "deleteButton");     // setting class name
+            deleteButton.setAttribute("value", key);
+            // createList.append(deleteButton);                     // inserting this button into list
+
+            let bothButtonDiv = document.createElement('div');
+            bothButtonDiv.setAttribute("class", "editDelteDiv");     // setting class name
+            bothButtonDiv.append(updateButton);
+            bothButtonDiv.append(deleteButton);
+            createList.append(bothButtonDiv);
+            // console.log(bothButtonDiv);
+
+            todoList.append(createList); // inserting list into UL
+            // Creating List End
+        }
+
+    })
 }
