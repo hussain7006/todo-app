56deb0ed619d40a08b59d0bd1e3530eb1991334a
hussain7006 <m.hussain7006@gmail.com>
2020-09-14 21:57:47 +0500

    add authentication
    not completed

---
+++ public/signUp.js
@@ -0,0 +1,30 @@
+let signUp = ()=> {
+    // alert("husssain");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+
+    console.log(email.value);
+    console.log(password.value);
+
+    if(email.value && password.value){
+
+        firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+        .then((result)=>{
+            console.log(result);
+
+            email.value = "";
+            password.value = "";
+        })
+        .catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            alert(errorMessage);
+            console.log(errorMessage)
+            // ...
+        });
+    }
+    else{
+        alert("Please Fill All Fields");
+    }
+}
